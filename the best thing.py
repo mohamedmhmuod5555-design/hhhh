c2b914328d8bf78b983f4277db1b55b610756391
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-28 06:39:34 -0700

    Update the best thing.py

--- the best thing.py
+++ the best thing.py
@@ -10,7 +10,7 @@ if 'num' not in st.session_state:
 if 'sc' not in st.session_state:
   st.session_state.sc=0
  if 'start_time' not in st.session_state:
-  st.session_state.start_time=time.Time()
+   st.session_state.start_time=time.Time()
 if 'count' not in st.session_state:
   st.session_state.count=0
 if 'num1' not in st.session_state:
