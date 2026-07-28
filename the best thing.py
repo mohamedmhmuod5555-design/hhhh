f022eec4427a9565ce4d814d15999407cf3594f6
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-28 07:21:47 -0700

    Update the best thing.py

--- the best thing.py
+++ the best thing.py
@@ -38,11 +38,11 @@ if st.button("تأكيد التخمين "):
    if taken >10:
       st.error("للاسف انتهي الوقت لقد خسرت اعد اللعبه وانا واثق بانك ستفوز ")
       st.session_state.num = 0
-  else : 
-      if number == sc:
-       st.success("اجابتك صحيحه ! لقد احسنت")
-       st.session_state.num += 1
-      else:
+   else : 
+       if number == sc:
+        st.success("اجابتك صحيحه ! لقد احسنت")
+        st.session_state.num += 1
+       else:
          st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
          st.session_state.num = 0
 if st.button("السؤال التالي "):
