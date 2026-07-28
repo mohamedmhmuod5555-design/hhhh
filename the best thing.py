f4811e69bc49532670bf1b7d24ec99ec25f1c803
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-28 07:19:38 -0700

    Update the best thing.py

--- the best thing.py
+++ the best thing.py
@@ -34,7 +34,7 @@ st.write(num1,sign,num2)
 number=st.number_input("ادخل النتيجه ")
 if st.button("تأكيد التخمين "):
    taken=time.time()- st.session_state.start_time
-  st.session_state.count += 1
+    st.session_state.count += 1
   if taken >10:
      st.error("للاسف انتهي الوقت لقد خسرت اعد اللعبه وانا واثق بانك ستفوز ")
      st.session_state.num = 0
